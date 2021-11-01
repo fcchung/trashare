d12c67fae6c1b839cc152b658a235a0ca20ee9e3
Fengrui Gan <fengruigan@gmail.com>
2021-10-31 20:45:46 -0700

    add database connection pool, connect user routes to database

---
+++ db/ConnectionPool.js
@@ -0,0 +1,49 @@
+/*
+  this is a module pattern for mongodb client and database
+  it should return a method to retrieve the same database instance
+  for all routes
+*/
+const { MongoClient } = require("mongodb");
+require("dotenv").config();
+
+const uri = process.env.DB_URL;
+const DB_NAME = "trashare";
+
+function ConnectionPool() {}
+
+ConnectionPool.db = null;
+ConnectionPool.client = null;
+
+ConnectionPool.prototype.connect = async function (dbName) {
+  if (ConnectionPool.isConnected) {
+    return;
+  }
+  console.log("Establishing connection to db");
+  if (ConnectionPool.client === null) {
+    ConnectionPool.client = new MongoClient(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      keepAlive: true,
+      maxPoolSize: 5,
+    });
+  }
+  await ConnectionPool.client.connect();
+  if (ConnectionPool.db === null) {
+    ConnectionPool.db = ConnectionPool.client.db(dbName);
+  }
+  ConnectionPool.isConnected = true;
+};
+
+ConnectionPool.prototype.getDB = function () {
+  return ConnectionPool.db;
+};
+
+ConnectionPool.prototype.getClient = function () {
+  return ConnectionPool.client;
+};
+
+module.exports = async () => {
+  let conn = new ConnectionPool();
+  await conn.connect(DB_NAME);
+  return conn;
+};
--- db/databaseManager.js
+++ db/databaseManager.js
@@ -1,7 +1,25 @@
-const databaseConnection = require("./databaseConnection");
+// const { MongoClient } = require("mongodb");
+const ConnectionPool = require("./ConnectionPool");
+
+// const dbOp = async (callback, ...params) => {
+//   let client;
+//   try {
+//     client = new MongoClient(process.env.DB_URL, {
+//       useNewUrlParser: true,
+//       useUnifiedTopology: true,
+//     });
+
+//     await client.connect();
+//     const db = client.db("trashare");
+//     callback.call(null, db, ...params);
+//   } finally {
+//     client.close();
+//   }
+// };
 
 const create = async (collectionName, data) => {
-  const db = await databaseConnection();
+  const conn = await ConnectionPool();
+  const db = conn.getDB();
   const collection = db.collection(collectionName);
   await collection.insertOne(data);
 };
@@ -10,7 +28,8 @@ const read = async (collectionName, query) => {
   if (typeof query !== "object") {
     throw new TypeError("Query Expression is not an object");
   }
-  const db = await databaseConnection();
+  const conn = await ConnectionPool();
+  const db = conn.getDB();
   const collection = db.collection(collectionName);
   let res = await collection.find(query).toArray();
   return res;
@@ -20,7 +39,8 @@ const update = async (collectionName, filter) => {
   if (typeof filter !== "object") {
     throw new TypeError("Filter Expression is not an object");
   }
-  const db = await databaseConnection();
+  const conn = await ConnectionPool();
+  const db = conn.getDB();
   const collection = db.collection(collectionName);
   await collection.updateOne(filter);
 };
@@ -29,7 +49,8 @@ const destroy = async (collectionName, filter) => {
   if (typeof filter !== "object") {
     throw new TypeError("Query Expression is not an object");
   }
-  const db = await databaseConnection();
+  const conn = await ConnectionPool();
+  const db = conn.getDB();
   const collection = db.collection(collectionName);
   await collection.deleteOne(filter);
 };
